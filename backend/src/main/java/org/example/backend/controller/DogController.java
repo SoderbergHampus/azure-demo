334e81d043650bb3663522ce43f5d1541cda44fd
SoderbergHampus <soderberg.dev@gmail.com>
2024-03-21 15:32:05 +0100

    Allowed cross origin in dog controller

--- backend/src/main/java/org/example/backend/controller/DogController.java
+++ backend/src/main/java/org/example/backend/controller/DogController.java
@@ -3,12 +3,14 @@ package org.example.backend.controller;
 import org.example.backend.DTOs.DogListDTO;
 import org.example.backend.service.DogService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/dogs")
+@CrossOrigin
 public class DogController {
 
     private final DogService service;
